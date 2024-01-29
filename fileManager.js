483378aae60c74d8fe1fc0ae811fe5d5b7693a8e
Nikita <nikitosminsk100@gmail.com>
2024-01-29 18:38:42 +0300

    feat: implement some nwd methods

--- fileManager.js
+++ fileManager.js
@@ -1,15 +1,19 @@
 import os from 'os';
-import { createInterface } from 'readline/promises';
+import readline from 'readline';
+import path from 'path';
+import fs from 'fs';
+
 
 class FileManager {
   #currentDirectory;
 
   constructor() {
     this.#currentDirectory = os.homedir();
+    console.info(`Initial directory: ${this.#currentDirectory}`);
   }
 
   async run() {
-    const read = createInterface({
+    const read = readline.promises.createInterface({
       input: process.stdin,
       output: process.stdout
     });
@@ -22,12 +26,16 @@ class FileManager {
 
       switch (command) {
         case 'up': {
+          this.#up();
           break;
         }
         case 'cd': {
+          const enteredPath = splittedInput.at(1);
+          await this.#cd(enteredPath);
           break;
         }
         case 'ls': {
+          await this.#ls();
           break;
         }
         case 'cat': {
@@ -54,8 +62,66 @@ class FileManager {
         case 'decompress': {
           break;
         }
+        case '.exit': {
+          read.close();
+          return;
+        }
       }
     }
+  }
+
+  #up() {
+    this.#currentDirectory = path.resolve(this.#currentDirectory, '../');
+  }
+
+  async #cd(enteredPath) {
+    let absolutePath;
+
+    if (path.isAbsolute(enteredPath)) {
+      absolutePath = enteredPath;
+    } else {
+      absolutePath = path.resolve(this.#currentDirectory, enteredPath);
+    }
+
+    const doesPathExist = FileManager.#doesPathExist(absolutePath);
+    const isFolder = FileManager.#isDirectory(absolutePath);
+
+    if (!isFolder) {
+      console.error('Can\'t move into non-directory');
+      return;
+    }
+
+    if (doesPathExist) {
+      this.#currentDirectory = absolutePath;
+    } else {
+      console.error('Path does not exist')
+    }
+  }
+
+  async #ls() {
+    const list = new Promise(resolve => {
+      fs.readdir(this.#currentDirectory, resolve);
+    });
+
+    const directories = await Promise.all(list.map(entity => {
+      return new Promise(resolve => {
+        FileManager.#isDirectory(entity).then(resolve);
+      });
+    }));
+  }
+
+  static async #isDirectory(absolutePath) {
+    const stats = await new Promise(resolve => {
+      fs.lstat(absolutePath, (error, stats) => resolve(error))
+    });
+
+    return stats.isDirectory();
+  }
+
+  static async #doesPathExist(absolutePath) {
+    return await new Promise(resolve => {
+      fs.exists(absolutePath, resolve);
+    });
   }
 }
 
