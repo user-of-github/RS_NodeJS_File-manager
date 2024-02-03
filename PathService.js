b84ad43ab4c58677ca3853e3912c286ee1aab302
Nikita <nikitosminsk100@gmail.com>
2024-02-03 21:09:44 +0300

    feat: PathService::extractPath + tests

--- PathService.js
+++ PathService.js
@@ -12,7 +12,47 @@ export class PathService {
     return path.resolve(dirname, relative);
   }
 
-  static extractFilePathFromString(fromInput) {
-    return fromInput.join(' ');
+  /**
+   PathService::extractFilePathFromString() receives a string and expected number of paths inside it
+   If path contains spaces, it must be wrapped with quotes, for example: cp "./path-to-file/file name with space.txt".
+   But if we are expecting only one file in function, for example cat new file.txt -- then we can not use quotes.
+   So quotes in required as wrapper for path/filename only if >= 2 entities are expected as arguments.
+   So acceptable combinations for entering paths:  cp "folder 1" "folder 2", cp folder-no-space "folder 2", cp "folder space" folder-no-space, cp folder-no-space1 no-space2
+   */
+  static extractFilePathFromString(wholeStringFromInputSource, expectedPathsCount) {
+    const longPathsDelimiter = '"';
+    const basePathsDelimiter = ' ';
+
+    const wholeStringFromInput = wholeStringFromInputSource.trim();
+    const splittedByQuotes = PathService.#splitPathsByDelimiter(wholeStringFromInput, longPathsDelimiter);
+
+    if (expectedPathsCount === 1) {
+      if (splittedByQuotes.length !== 1) {
+        return {parseStatusSuccess: false, paths: []};
+      }
+
+      return {parseStatusSuccess: true, paths: splittedByQuotes};
+    }
+
+    if (splittedByQuotes.length === 1) {
+      // No long paths detected, for example: cp ./file1 ./file2  ==> split by space
+      const paths = PathService.#splitPathsByDelimiter(wholeStringFromInput, basePathsDelimiter);
+      if (paths.length !== expectedPathsCount) {
+        return {parseStatusSuccess: false, paths: []};
+      }
+
+      return { parseStatusSuccess: true, paths};
+    }
+
+    // one or all paths are in quotes, for example: cp "./file.txt" file2.txt
+    if (splittedByQuotes.length !== expectedPathsCount) {
+      return {parseStatusSuccess: false, paths: []};
+    }
+
+    return {parseStatusSuccess: true, paths: splittedByQuotes}
+  }
+
+  static #splitPathsByDelimiter(input, delimiter) {
+    return input.split(delimiter).filter(path => path.trim() !== '').map(item => item.trim());
   }
 }
---
+++ PathService.test.js
@@ -0,0 +1,39 @@
+import {PathService} from './PathService.js';
+
+/**
+ PathService::extractFilePathFromString() receives a string and expected number of paths inside it
+ If path contains spaces, it must be wrapped with quotes, for example: cp "./path-to-file/file name with space.txt".
+ But if we are expecting only one file in function, for example cat new file.txt -- then we can not use quotes.
+ So quotes in required as wrapper for path/filename only if >= 2 entities are expected as arguments.
+ So acceptable combinations for entering paths:  cp "folder 1" "folder 2", cp folder-no-space "folder 2", cp "folder space" folder-no-space, cp folder-no-space1 no-space2
+ */
+
+const testCases = [
+  [['"path-in-quotes"  path-without-quotes', 2], {parseStatusSuccess: true, paths: ['path-in-quotes', 'path-without-quotes']}],
+  [['"./long path with space/path-in-quotes.txt"  path-without-quotes', 2], {parseStatusSuccess: true, paths: ['./long path with space/path-in-quotes.txt', 'path-without-quotes']}],
+  [['"./long path with space/path-in-quotes.txt"  path-without-quotes', 1], {parseStatusSuccess: false, paths: []}],
+  [[' "./long path with space/path-in-quotes.txt"  ', 1], {parseStatusSuccess: true, paths: ['./long path with space/path-in-quotes.txt']}],
+  [[' "./long path with space/path-in-quotes.txt"  ', 2], {parseStatusSuccess: false, paths: []}],
+  [[' "./long path with space/path-in-quotes.txt"  ', 3], {parseStatusSuccess: false, paths: []}],
+  [[' "./long path with space/path-in-quotes.txt"  "../../another-path.txt"', 2], {parseStatusSuccess: true, paths: ['./long path with space/path-in-quotes.txt', '../../another-path.txt']}],
+  [[' "./long path with space/path-in-quotes.txt"  "../../another-path.txt"', 1], {parseStatusSuccess: false, paths: []}],
+  [['./long path with space/path-in-quotes.txt', 1], {parseStatusSuccess: true, paths: ['./long path with space/path-in-quotes.txt']}],
+  [['./long path with space/path-in-quotes.txt ./another-long-path/file with space.txt', 2], {parseStatusSuccess: false, paths: []}],
+  [['./long path with space/path-in-quotes.txt ./another-long-path/file with space.txt', 1], {parseStatusSuccess: true, paths: ['./long path with space/path-in-quotes.txt ./another-long-path/file with space.txt']}],
+  [['"folder 1" "./folder 2/file"', 2], {parseStatusSuccess: true, paths: ['folder 1', './folder 2/file']}],
+];
+
+const test = () => {
+  testCases.forEach((testCase, index) => {
+    console.log(`Test case #${index}`);
+
+    const input = testCase[0];
+    const expectedResult = testCase[1];
+    const receivedResult = PathService.extractFilePathFromString(...input);
+
+    console.assert(JSON.stringify(expectedResult) === JSON.stringify(receivedResult));
+  });
+};
+
+
+test();
